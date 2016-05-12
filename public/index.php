f68b2b240809249e9f3fdd5078f57a31de5ea26e
Devin McBeth <chromiem@gmail.com>
2016-05-12 12:10:32 -0700

    Set session cookie to 15 days  - to extend the login time past just the current session

--- public/index.php
+++ public/index.php
@@ -6,6 +6,10 @@ define('APP_PATH', '../');
 // Require composer auto loader
 require_once APP_PATH . 'vendor/autoload.php';
 
+// Set the session cookie lifetime to 15 days
+$session_lifetime = 60 * 60 * 24 * 15;
+session_set_cookie_params($session_lifetime);
+
 // Require session on every request
 session_start();
 
